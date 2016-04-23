d819f7a1133194bbdff020901e90192ca4ca8038
kaukassus <michael.burgherr@outlook.com>
2016-04-23 21:25:58 +0200

    Adjust Build Script and mission.sqm for new version.

--- src-tools/comfy_make/src/main.rs
+++ src-tools/comfy_make/src/main.rs
@@ -10,7 +10,7 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-  let version: u8 = 7;  // Version that gets printed into the PBO filename
+  let version: u8 = 8;  // Version that gets printed into the PBO filename
 
   let worlds = vec![
     "VR",
@@ -35,11 +35,9 @@ fn main() {
   ];
 
   let cwd: String = get_cwd();
-  prepare_adv_medcial();
 
   for map in worlds {
     compile(cwd.as_ref(), version, map);
-    compile_adv_medical(cwd.as_ref(), version, map);
   }
 }
 
@@ -60,35 +58,4 @@ fn compile(path: &str,version: u8, map: &str) {
   let dirpath: String = format!("{}FP_ComfyDowntime.VR", path);
   let newpath: String = format!("{}FP_ComfyDowntime_{}.{}.pbo", path, version, map);
   native::run("PBOConsole", &["-pack", dirpath.as_ref(), newpath.as_ref()]);
-}
-
-/*
-  Function that Generates the new Filename with version info and map postfix
-  and call to PBOConsole for the Advanced Medical Mission.
-*/
-fn compile_adv_medical(path: &str, version: u8, map: &str) {
-  let dirpath: String = format!("{}FP_ComfyDowntime_ADV.VR", path);
-  let newpath: String = format!("{}FP_ComfyDowntime_ADV_{}.{}.pbo", path, version, map);
-  native::run("PBOConsole", &["-pack", dirpath.as_ref(), newpath.as_ref()]);
-}
-
-/*
-  Function that Prepares the ADV medical Folder to be built.
-  This may be obsolete once ADV Medical gets incorporated into the main mission File.
-*/
-fn prepare_adv_medcial() {
-  
-  for entry in WalkDir::new("FP_ComfyDowntime.VR") {
-    let entry = entry.unwrap();
-    let entry_path: &Path = entry.path();
-    let pathbuf = entry_path.to_path_buf();
-    let newpath_string: String = pathbuf.to_str().unwrap().to_owned();
-    let newpath_string_replaced: String = newpath_string.replace("FP_ComfyDowntime", "FP_ComfyDowntime_ADV");
-    let newpath_string_replaced_slice: &str = newpath_string_replaced.as_ref();
-    let newpath = Path::new(newpath_string_replaced_slice);
-    if entry_path.is_dir() {fs::create_dir(&newpath);} else {fs::copy(&entry_path, &newpath);}
-  }
-
-  // Copy over the Advanced medical mission file
-  fs::copy("mission_adv_medical.sqm", "FP_ComfyDowntime_ADV.VR/mission.sqm");
 }
