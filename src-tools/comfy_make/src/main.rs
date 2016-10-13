6e795160bdadb8eb21d03b6c7b797b01e545bba1
Comfy <comfy@comfy.li>
2016-10-13 21:46:44 +0200

    Fix missing paa file, update build and make builder use makepbo from mikeros pbo tools.

--- src-tools/comfy_make/src/main.rs
+++ src-tools/comfy_make/src/main.rs
@@ -10,28 +10,36 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-  let version: u8 = 8;  // Version that gets printed into the PBO filename
+  let version: u8 = 10;  // Version that gets printed into the PBO filename
 
   let worlds = vec![
-    "VR",
-    "Sara",
-    "SaraLite",
-    "Sara_dbe1",
-    "Stratis",
+    "abel",
+    "Altis",
+    "Bootcamp_ACR",
+    "cain",
     "Chernarus",
     "Chernarus_Summer",
+    "Desert_E",
+    "Desert_Island",
+    "eden",
+    "mbg_celle2",
+    "Mountains_ACR",
+    "Napf",
+    "NapfWinter",
+    "noe",
+    "Porto",
+    "ProvingGrounds_PMC",
+    "Sara",
+    "Sara_dbe1",
+    "SaraLite",
+    "Shapur_BAF",
+    "Stratis",
     "Takistan",
+    "Tanoa",
     "utes",
-    "Zargabad",
-    "reshmaan",
-    "MCN_Aliabad",
-    "IsolaDiCapraia",
-    "fata",
-    "Caribou",
-    "Altis",
-    "anim_helvantis_v2",
+    "VR",
     "Woodland_ACR",
-    "ProvingGrounds_PMC"
+    "Zargabad"
   ];
 
   let cwd: String = get_cwd();
@@ -57,5 +65,6 @@ fn get_cwd() -> String {
 fn compile(path: &str,version: u8, map: &str) {
   let dirpath: String = format!("{}FP_ComfyDowntime.VR", path);
   let newpath: String = format!("{}FP_ComfyDowntime_{}.{}.pbo", path, version, map);
-  native::run("PBOConsole", &["-pack", dirpath.as_ref(), newpath.as_ref()]);
+  //native::run("PBOConsole", &["-pack", dirpath.as_ref(), newpath.as_ref()]);
+  native::run("makepbo", &["-N", "-P", dirpath.as_ref(), newpath.as_ref()]);
 }
